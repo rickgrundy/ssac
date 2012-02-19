56132298ec3f3d6e55326e6aeffa4be0806b048c
Rick Grundy <rick@rickgrundy.com>
2012-02-19 16:54:41 +0800

    Membership info page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def committee_info_path
+    membership_info_path(anchor: "committee")
+  end  
+  
+  def get_property(name)
+    raw "<span class='error'>[MISSING PROPERTY #{name}]</span>"
+  end
 end
