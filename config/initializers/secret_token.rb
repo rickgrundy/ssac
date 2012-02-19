e29985b2c885292dda5bd88e6b33956889c159bc
Rick Grundy <rick@rickgrundy.com>
2012-02-19 09:32:10 +0800

    Skeleton Rails 3.2 app with rspec and postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ssac::Application.config.secret_token = 'db3a5ee273cae8d04148be3e0ab1b0e63a35c5cc8331db62b8a4b7ac707ebc1f94b61b3e07bf3e59d90d9fa6b8fdbcded5a184f73972a14c299b090aa482daa5'
