5a695b7a83df346cb23987cddbe720c32bbe4722
Rick Grundy <rick@rickgrundy.com>
2012-02-21 00:01:28 +0800

    Form style tweak.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def index
-    @items = TripReport.recent.includes(:photos).first(3)
+    @items = TripReport.recent.includes(:photos).first(4)
     @photos = Photo.includes(:trip_report).first(6)
   end
 end
