898ee136db0cfef33172f81a5c249e637eedad70
Rick Grundy <rick@rickgrundy.com>
2012-02-20 23:57:29 +0800

    Required a secret question to be answered on signup to keep spam-bots out.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   validates_presence_of :name
+  attr_accessor :ssac_secret_question
+  validates :ssac_secret_question, on: :create, inclusion: {in: [ENV["SSAC_SECRET_QUESTION"]], message: "answer is not correct."}
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :ssac_secret_question
   
   def can_edit?(record)
     record.user == self || self.administrator
+  end
+  
+  private
+  
+  def ssac_secret_question
+    
   end
 end
