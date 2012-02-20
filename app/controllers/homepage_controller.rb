8c71713b3f86750b5858a702137f8173f57929da
Rick Grundy <rick@rickgrundy.com>
2012-02-20 23:44:41 +0800

    Fiddling with viewport for ipad and mobiles.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def index
-    @items = TripReport.recent.includes(:photos).first(6)
-    @photos = Photo.includes(:trip_report).first(4)
+    @items = TripReport.recent.includes(:photos).first(3)
+    @photos = Photo.includes(:trip_report).first(6)
   end
 end
