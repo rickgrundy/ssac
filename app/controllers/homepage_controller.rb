04e01a02732e04702787a1d2ceefa8996834232f
Rick Grundy <rick@rickgrundy.com>
2012-02-23 19:57:30 +0800

    Removed URL from banner to keep Ben happy

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,6 @@ class HomepageController < ApplicationController
     @trip_reports = TripReport.recent.includes(:photos).first(3)
     @planned_dives = PlannedDive.future.includes(:user).first(3)
     @items = (@trip_reports + @planned_dives).sort_by(&:created_at).reverse
-    @photos = Photo.randomised.includes(:trip_report).first(7)
+    @photos = Photo.randomised.includes(:trip_report).first(5)
   end
 end
