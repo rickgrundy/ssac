3eed8335844464ed8b492c439307923c91a7d646
Rick Grundy <rick@rickgrundy.com>
2012-02-27 18:43:35 +0800

    Fixed position of plan a new dive button.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,6 +3,6 @@ class HomepageController < ApplicationController
     @trip_reports = TripReport.recent.includes(:photos).first(3)
     @planned_dives = PlannedDive.future.includes(:user).first(3)
     @items = (@trip_reports + @planned_dives).sort_by(&:created_at).reverse
-    @photos = Photo.randomised.includes(:trip_report).first(5)
+    @photos = Photo.randomised.first(6)
   end
 end
