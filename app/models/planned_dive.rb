7aaaa176424971329d0a04ddd5c1fd96df9261cb
Rick Grundy <rick@rickgrundy.com>
2012-02-27 18:58:24 +0800

    Removed requirement for number of places. Don't render Who's coming title unless divers exist.

--- app/models/planned_dive.rb
+++ app/models/planned_dive.rb
@@ -1,6 +1,6 @@
 class PlannedDive < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :event_type, :start_date, :destination, :description, :number_of_spaces, :suitable_for
+  validates_presence_of :event_type, :start_date, :destination, :description, :suitable_for
   scope :future, conditions: ["start_date > ?", Date.today - 1], order: "start_date ASC"
   
   extend FriendlyId
