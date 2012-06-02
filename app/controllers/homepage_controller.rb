c3e78e144e6e7481e89fd02de52e9d9106375d51
Rick Grundy <rick@rickgrundy.com>
2012-06-02 10:29:18 +0800

    +1 photo on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,8 +1,11 @@
 class HomepageController < ApplicationController
+  PHOTO_COUNT = 8
+  TRIP_REPORT_COUNT = 3
+  PLANNED_DIVE_COUNT = 3
   def index
-    @trip_reports = TripReport.recent.includes(:photos).first(3)
-    @planned_dives = PlannedDive.future.includes(:user).first(3)
+    @trip_reports = TripReport.recent.includes(:photos).first(TRIP_REPORT_COUNT)
+    @planned_dives = PlannedDive.future.includes(:user).first(PLANNED_DIVE_COUNT)
     @items = (@trip_reports + @planned_dives).sort_by(&:created_at).reverse
-    @photos = Photo.starred.randomised.first(7)
+    @photos = Photo.starred.randomised.first(PHOTO_COUNT)
   end
 end
