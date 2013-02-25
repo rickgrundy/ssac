0ba6e44ac4598145cd1bc2e5ef3510b983e3714a
Rick Grundy <rick@rickgrundy.com>
2013-02-25 12:33:48 +1100

    Fixed misuse of new committee members on membership and planned dives pages

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,13 +1,16 @@
 class Committee
   class Position < Struct.new(:title, :email, :name)
+    def first_name
+      name.split.first
+    end
   end
   
-  POSITIONS = [
-    Position.new('Chairman',            'chairman@sydneysubaquaclub.com',      'Ben Crossett'),
-    Position.new('Diving Officer',      'divingofficer@sydneysubaquaclub.com', 'Paul Furniss'),
-    Position.new('Secretary',           'secretary@sydneysubaquaclub.com',     'Stuart Marston'),
-    Position.new('Treasurer',           'finance@sydneysubaquaclub.com',       'Robert Larisch'),
-    Position.new('Expeditions Officer', 'diving@sydneysubaquaclub.com',        'Gary Meakin'),
-    Position.new('Web Officer',         'web@sydneysubaquaclub.com',           'Rick Grundy')
-  ]
+  CHAIRMAN = Position.new('Chairman', 'chairman@sydneysubaquaclub.com', 'Ben Crossett')
+  DIVING_OFFICER = Position.new('Diving Officer', 'divingofficer@sydneysubaquaclub.com', 'Paul Furniss')
+  SECRETARY = Position.new('Secretary', 'secretary@sydneysubaquaclub.com', 'Stuart Marston')
+  TREASURER = Position.new('Treasurer', 'finance@sydneysubaquaclub.com', 'Robert Larisch')
+  EXPEDITIONS_OFFICER = Position.new('Expeditions Officer', 'diving@sydneysubaquaclub.com', 'Gary Meakin')
+  WEB_OFFICER = Position.new('Web Officer', 'web@sydneysubaquaclub.com', 'Rick Grundy')
+  
+  POSITIONS = [CHAIRMAN, DIVING_OFFICER, SECRETARY, TREASURER, EXPEDITIONS_OFFICER, WEB_OFFICER]
 end
