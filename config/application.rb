6d446c9e6f9cdab170d381f87d9847e183f39543
Rick Grundy <rick@rickgrundy.com>
2014-07-20 10:09:25 +1000

    Are the frequent invalid UTF-8 errors in the heroku logs somehow causing the H12 timeouts?

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../boot', __FILE__)
+require File.expand_path('../utf8_sanitizer', __FILE__)
 
 require 'rails/all'
 
@@ -58,5 +59,7 @@ module Ssac
     config.assets.version = '1.0'
     
     config.action_mailer.default_url_options = { :host => "sydneysubaquaclub.com" }
+    
+    config.middleware.use Utf8Sanitizer
   end
 end
