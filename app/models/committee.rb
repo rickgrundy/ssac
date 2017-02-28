a82648e8efd36fe4647bdcd47d7ca768df35e7a9
Rick Grundy <rick@rickgrundy.com>
2017-02-28 19:02:38 +1100

    2017 committee

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -4,13 +4,12 @@ class Committee
       name.split.first
     end
   end
-  
+
   CHAIRMAN = Position.new('Chairman', 'chairman@sydneysubaquaclub.com', 'Soo Parkinson')
-  DIVING_OFFICER = Position.new('Diving Officer', 'divingofficer@sydneysubaquaclub.com', 'Ross Coleman')
+  DIVING_OFFICER = Position.new('Diving Officer', 'divingofficer@sydneysubaquaclub.com', 'Andy Garvey')
   SECRETARY = Position.new('Secretary', 'secretary@sydneysubaquaclub.com', 'Sam Warwick')
-  TREASURER = Position.new('Treasurer', 'finance@sydneysubaquaclub.com', 'Stuart Marston')
+  TREASURER = Position.new('Treasurer', 'finance@sydneysubaquaclub.com', 'Ben Crossett')
   EXPEDITIONS_OFFICER = Position.new('Expeditions Officer', 'diving@sydneysubaquaclub.com', 'Fe Edwards')
-  WEB_OFFICER = Position.new('Web Officer', 'web@sydneysubaquaclub.com', 'Rick Grundy')
-  
-  POSITIONS = [CHAIRMAN, DIVING_OFFICER, SECRETARY, TREASURER, EXPEDITIONS_OFFICER, WEB_OFFICER]
+
+  POSITIONS = [CHAIRMAN, DIVING_OFFICER, SECRETARY, TREASURER, EXPEDITIONS_OFFICER]
 end
